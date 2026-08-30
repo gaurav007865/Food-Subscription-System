cd5795a1b8b7a1460aece32ee94f5600d6fab841
Mrunali <mrunalibisen897@gmail.com>
2026-08-30 22:23:35 +0530

    Added data in sheet NGOMembers

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 // ==========================================================================
 // GLOBAL CONFIG - Single source of truth for the backend URL
 // ==========================================================================
-const API_URL = "https://script.google.com/macros/s/AKfycbzXsar5OoCBV3WqcvZVglYoOVdMMi6B8y0TcG6YoXdreuHdIyjxh2Cq1Zxqc_otib7tDg/exec"
+const API_URL = "https://script.google.com/macros/s/AKfycbwM9iRaGDyq_l7yhUXMzamXQ4ufehQ74sdpS3EKSnADs_y6O_vc-0LpzZ9dplOL9L8Glg/exec"
